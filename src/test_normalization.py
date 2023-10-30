a8db8c2a03967713eb5e82404e731816e58f0902
matheusja <matheusjahnke@hotmail.com>
2023-10-30 17:22:08 -0300

    Now running for every LF in training data

--- src/test_normalization.py
+++ src/test_normalization.py
@@ -1,10 +1,22 @@
 import dataset_reader
 import numpy
 import einops
-img = dataset_reader.read_LF("/scratch/Original_LFs/png/Urban/Bench_in_Paris.mat.png")
-dataset_reader.write_LF_PMG(img.numpy(), "Bench_in_Paris.mat")
-img2 = dataset_reader.read_LF("Bench_in_Paris.mat.png")
 
-diff = img - img2
-print(einops.reduce(diff, '... -> ', 'max'))
-print(einops.reduce(diff, '... -> ', 'min'))
+def test(img, label):
+    dataset_reader.write_LF_PMG(img.numpy(), "example")
+    img2 = dataset_reader.read_LF("example.png")
+    diff = img - img2
+    MAX = (einops.reduce(diff, '... -> ', 'max'))
+    MIN = (einops.reduce(diff, '... -> ', 'min'))
+    print(f"{label}\t{MAX}\t{MIN}")
+
+dataset = dataset_reader.training_dataset
+for lf in dataset.lfs:
+    img = dataset.read_original_mat(*lf)
+    test(img, '_'.join(lf))
+
+    bpps = dataset.bbps[lf[0], lf[1]]
+    for bpp in bpps:
+        (bpp, img) = dataset.read_decoded_mat(*lf, bpp)
+        test(img, bpp)
+
